69ebf9d09e717ab48c88e307b56ddb9cc0abf5ff
Viktar Feshchanok <vituok.ph@gmail.com>
2024-10-14 23:38:02 -0400

    lisener('focus') & passwordConfirm

--- Log In : new account/javascript.js
+++ Log In : new account/javascript.js
@@ -20,7 +20,7 @@ const registrationPage = document.getElementById(pages.registration)
 const welcomePage = document.getElementById(pages.welcome)
 
 
-let  log_inputs = []
+let  logInputs = []
 
 if(page === `?${pages.login}`) {
     welcomePage.classList.add("hidden");
@@ -28,14 +28,14 @@ if(page === `?${pages.login}`) {
     loginPage.classList.remove("hidden")
    
     
-    log_inputs = [
+    logInputs = [
 
-        {name: "login", 
-            element: document.getElementById("log_login")
+        {name: "logLogin", 
+            element: document.getElementById("logLogin")
         },
     
-        {name: "log_password", 
-            element: document.getElementById("log_password")
+        {name: "logPassword", 
+            element: document.getElementById("logPassword")
         }
     
     ]
@@ -46,17 +46,17 @@ if(page === `?${pages.login}`) {
 const logInBtn = document.getElementById("logInBtn")
 logInBtn.addEventListener("click",()=>{
 
-    const logPasswordValue = log_inputs.find(item => item.name === "log_password").element.value;
+    const logPassword = logInputs.find(item => item.name === "logPassword").element;
 
 
-    const logError = log_inputs.some((i)=> !i.element.value)
+    const logError = logInputs.some((i)=> !i.element.value)
     
     if (logError) {
         return;
     }
 
-    if (logPasswordValue.length < 8){
-        log_inputs.find(item => item.name === "log_password").element.classList.add('empty')
+    if (logPassword.length < 8){
+        logPassword.classList.add('empty')
        return;
     }
     window.location.search = '?welcome';
@@ -70,7 +70,13 @@ window.location.search = '?registration';
 })
 
 
+
+
+
+
 let inputs = []
+
+
 
 
 if(page === `?${pages.registration}`) {
@@ -100,10 +106,22 @@ if(page === `?${pages.registration}`) {
             name: "password", 
             element: document.getElementById("password")
         },
+        {
+        name: "passwordConfirm", 
+        element: document.getElementById("passwordConfirm")
+    }
         
     ]
 
 }
+
+inputs.forEach((input) => {
+input.element.addEventListener('focus', deleteEmpty)
+ function deleteEmpty(){
+    (input.element.classList.remove("empty"))
+}
+})
+
 
 
 
@@ -111,29 +129,38 @@ const registrationBtn = document.getElementById("registrationBtn")
     registrationBtn.addEventListener("click",()=>{
 
 
-
     //если input пустой становится красным
 
+    const password = inputs.find(element => element.name === "password")
+    const passwordConfirm = inputs.find(element => element.name === "passwordConfirm")
+    passwordLenght = password.element.value.length
 
-    window.addEventListener("input" && "click",
-        function(){
-            inputs.forEach((value)=>{
-            value.element.value.length === 0 ? value.element.classList.add('empty') : value.element.classList.remove('empty')
-            })
-        });
-    
+    console.log()
 
+        inputs.forEach((value)=>{
+        value.element.value ? value.element.classList.remove('empty') : value.element.classList.add('empty')
+        
+        if (passwordLenght < 8 || password != passwordConfirm){
+            password.element.classList.add('empty')
+            passwordConfirm.element.classList.add('empty')
+        }       
+
+    })
+
+       
     const hasError = inputs.some((input) => !input.element.value)
-        if(hasError){
+        if(hasError || passwordLenght < 8||password.element.value != passwordConfirm.element.value) {
             return;
         }
 
-
-        console.log(inputs)
         
     window.location.search = '?login';
-    
 })
+
+
+
+
+
 
 if(page === `?${pages.welcome}`) {
     loginPage.classList.add("hidden")
